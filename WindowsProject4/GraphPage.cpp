5d9284b59824ae610cef750d9eb740ebc109ee09
Qwerty7310 <daniyl.2004@yandex.ru>
2024-05-02 23:24:07 +0300

    add GraphPage.cpp and GraphPage.h

--- WindowsProject4/GraphPage.cpp
+++ WindowsProject4/GraphPage.cpp
@@ -1,4 +1,238 @@
-#include "Header.h"
+//#pragma once
 
+#include "GraphPage.h"
 
+/*----------------------------------- Процедура окна -----------------------------------*/
 
+LRESULT CALLBACK GraphPageProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    //OutputDebugString(L"GraphPageProc 1\n");
+    //PAINTSTRUCT ps;
+    UINT state;
+    PAINTSTRUCT ps;
+
+    RECT rectClient;
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        hdc = BeginPaint(hWnd, &ps);
+        SetTextColor(hdc, 0x00FF0000); // синий цвет букв
+        DrawGraph(hdc, ps.rcPaint, x, NUM, 3); // построение графика
+        DrawTextOnGraphPage(hdc, ps.rcPaint); // текст
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_COMMAND:
+    {
+        // Получаем ID чекбокса, который был нажат
+        int wmId = LOWORD(wParam);
+        // Получаем дескриптор (HWND) чекбокса
+        HWND hwndCtl = (HWND)lParam;
+
+        // Обрабатываем нажатие на чекбокс в зависимости от его ID
+        switch (wmId)
+        {
+        case ID_CHECKBOX_1:
+            // Обработка нажатия на чекбокс 1
+            OutputDebugString(L"ID_CHECKBOX_1!\n");
+            // Получаем текущий статус чекбокса
+            state = SendMessage(hCheckBox1, BM_GETCHECK, 0, 0);
+
+            // Проверяем статус чекбокса
+            switch (state)
+            {
+            case BST_CHECKED: // Чекбокс нажат
+                // Сбрасываем состояние чекбокса
+                SendMessage(hCheckBox1, BM_SETCHECK, BST_UNCHECKED, 0);
+                break;
+            case BST_UNCHECKED: // Чекбокс не нажат
+                // Устанавливаем состояние чекбокса в "нажатое"
+                SendMessage(hCheckBox1, BM_SETCHECK, BST_CHECKED, 0);
+                break;
+            default:
+                // Неопределенный статус
+                break;
+            }
+            break;
+        case ID_CHECKBOX_2:
+            // Обработка нажатия на чекбокс 2
+            OutputDebugString(L"ID_CHECKBOX_2!\n");
+            // Получаем текущий статус чекбокса
+            state = SendMessage(hCheckBox2, BM_GETCHECK, 0, 0);
+
+            // Проверяем статус чекбокса
+            switch (state)
+            {
+            case BST_CHECKED: // Чекбокс нажат
+                // Сбрасываем состояние чекбокса
+                SendMessage(hCheckBox2, BM_SETCHECK, BST_UNCHECKED, 0);
+                break;
+            case BST_UNCHECKED: // Чекбокс не нажат
+                // Устанавливаем состояние чекбокса в "нажатое"
+                SendMessage(hCheckBox2, BM_SETCHECK, BST_CHECKED, 0);
+                break;
+            default:
+                // Неопределенный статус
+                break;
+            }
+            break;
+        case ID_CHECKBOX_3:
+            // Обработка нажатия на чекбокс 3
+            OutputDebugString(L"ID_CHECKBOX_3!\n");
+            // Получаем текущий статус чекбокса
+            state = SendMessage(hCheckBox3, BM_GETCHECK, 0, 0);
+
+            // Проверяем статус чекбокса
+            switch (state)
+            {
+            case BST_CHECKED: // Чекбокс нажат
+                // Сбрасываем состояние чекбокса
+                SendMessage(hCheckBox3, BM_SETCHECK, BST_UNCHECKED, 0);
+                break;
+            case BST_UNCHECKED: // Чекбокс не нажат
+                // Устанавливаем состояние чекбокса в "нажатое"
+                SendMessage(hCheckBox3, BM_SETCHECK, BST_CHECKED, 0);
+                break;
+            default:
+                // Неопределенный статус
+                break;
+            }
+            break;
+            // Добавьте другие случаи по мере необходимости
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    }
+    if (g_pGraphPageProc)
+        return CallWindowProc(g_pGraphPageProc, hWnd, message, wParam, lParam);
+    else
+        return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+/*------------------------------ Функция рисования графика ------------------------------*/
+
+void DrawGraph(HDC hdc, RECT rectClient, double** x, // массив данных
+    int n, // количество точек
+    int numrow = 1) // количество рядов данных (по умолчанию 1)
+{
+    SetBkMode(hdc, TRANSPARENT); // фон прозрачный
+    double OffsetY, OffsetX; double MAX_X, MAX_Y; double ScaleX, ScaleY;
+    double min, max; int height, width;
+    int X, Y; // координаты в окне (в рх)
+    HPEN hpen;
+    height = rectClient.bottom - rectClient.top;
+    width = rectClient.right - rectClient.left - 200;
+    // Область допустимых значений Х
+    min = x[0][0];
+    max = x[0][0];
+    for (int i = 0; i < n; i++)
+    {
+        if (x[0][i] < min) min = x[0][i];
+        if (x[0][i] > max) max = x[0][i];
+    }
+
+    double temp = max - min;
+    MAX_X = max - min; // Разброс по оси Х
+    OffsetX = min * width / MAX_X; // смещение X
+    ScaleX = (double)width / MAX_X; // масштабный коэффициент Х
+    // Область допустимых значений Y
+    max = 1.5; /////////!!!!!!!!!!!
+    min = -1.5; ///
+
+    MAX_Y = max - min; // Разброс по оси Y
+    OffsetY = max * height / (MAX_Y); // смещение Y
+    ScaleY = (double)height / MAX_Y; // масштабный коэффициент Y
+    // Отрисовка осей координат
+    hpen = CreatePen(PS_SOLID, 3, 0); // чёрное перо толщиной в 3 пикселя
+    SelectObject(hdc, hpen);
+    line(hdc, 10, OffsetY, width - 10, OffsetY); // рисование горизонтальной оси line(hdc, width - 10, OffsetY, width - 15, OffsetY - 5); // верхнее горизонтальное оперение line(hdc, width - 10, OffsetY, width - 15, OffsetY + 5); // нижнее горизонтальное оперение
+    for (int i = 0; i < 9; i++) line(hdc, i * 0.118 * width, OffsetY, i * 0.118 * width, OffsetY - 3);
+    // отметки на горизонтальной оси
+    line(hdc, OffsetX + 10, 0, OffsetX + 10, height - 5); // рисование вертикальной оси line(hdc, OffsetX + 10, 0, OffsetX + 15, 10); // рисование правого вертикального оперения line(hdc, OffsetX + 10, 0, OffsetX + 5, 10); // рисование левого вертикального оперения
+    for (int i = -2; i < 3; i++) line(hdc, OffsetX + 10, OffsetY + height / 6 * i, OffsetX + 15, OffsetY + height / 6 * i);
+    // отметки на вертикальной оси
+    DeleteObject(hpen); // удаление черного пера
+
+                // Отрисовка графика функции
+    int color = 0xFF0000; // синее перо для первого ряда данных
+    TextOut(hdc, 21, 7, _T("Y = sin (X)"), 13); // Вывод текста y=sin(x) 8 символов
+    TextOut(hdc, 27, height / 6 - 12, _T("1,0"), 3);
+    TextOut(hdc, 27, height / 3 - 12, _T("0,5"), 3);
+    TextOut(hdc, 27, 2 * height / 3 - 12, _T("-0,5"), 4);
+    TextOut(hdc, 27, 5 * height / 6 - 12, _T("-1,0"), 4);
+    TextOut(hdc, 15, 5 + 0.5 * height, _T("0,0"), 3);
+    TextOut(hdc, 0.92 * width * 0.125, 5 + 0.5 * height, _T("0,78"), 4);
+    TextOut(hdc, 0.92 * width * 0.25, 5 + 0.5 * height, _T("1,57"), 4);
+    TextOut(hdc, 0.92 * width * 0.375, 5 + 0.5 * height, _T("2,35"), 4);
+    TextOut(hdc, 0.92 * width * 0.5, 5 + 0.5 * height, _T("3,14"), 4);
+    TextOut(hdc, 0.92 * width * 0.625, 5 + 0.5 * height, _T("3,92"), 4);
+    TextOut(hdc, 0.92 * width * 0.75, 5 + 0.5 * height, _T("4,71"), 4);
+    TextOut(hdc, 0.92 * width * 0.875, 5 + 0.5 * height, _T("5,49"), 4);
+    TextOut(hdc, 0.92 * width, 5 + 0.5 * height, _T("6,28"), 4);
+    TextOut(hdc, 0.98 * width, 5 + 0.5 * height, _T("x"), 1);
+    for (int j = 1; j <= numrow; j++)
+    {
+        while (1)
+        {
+            hpen = CreatePen(PS_SOLID, 3, color); // формирование пера толщиной 3 пикселя
+            SelectObject(hdc, hpen);
+            X = (int)(OffsetX + x[0][0] * ScaleX); // начальная точка графика
+            Y = (int)(OffsetY - x[j][0] * ScaleY);
+            MoveToEx(hdc, X + 10, Y, 0); // перемещение в начальную точку
+            for (int i = 0; i < (n - 3); i++)
+            {
+                X = OffsetX + 12 + x[0][i] * ScaleX;
+                Y = OffsetY - x[j][i] * ScaleY;
+                LineTo(hdc, X, Y);
+            }
+            color = color >> 8; // изменение цвета пера для следующего ряда
+            DeleteObject(hpen); // удаление текущего пера
+            break;
+        }
+    }
+}
+
+void DrawTextOnGraphPage(HDC hdc, RECT rectClient) {
+    UINT state;
+
+    //Создание надписи
+    HFONT hFont = CreateFont(20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        DEFAULT_PITCH | FF_SWISS, L"Arial");
+
+    // серый цвет
+    SetTextColor(hdc, RGB(100, 100, 100));
+    SelectObject(hdc, hFont); // выбираем шрифт
+    SetBkMode(hdc, TRANSPARENT); // фон прозрачный
+
+    // Рисуем текст с прозрачным фоном
+    //TextOut(hdc, rectClient.right - 200, 20, L"Выберите графики,\nкоторые вы хотите увидеть:", wcslen(L"Выберите графики,\nкоторые вы хотите увидеть:"));
+
+    RECT rectText = { rectClient.right - 200, 20, rectClient.right, rectClient.bottom };
+    DrawText(hdc, L"Выберите графики,\nкоторые вы хотите увидеть:", -1, &rectText, DT_WORDBREAK);
+
+    state = SendMessage(hCheckBox1, BM_GETCHECK, 0, 0);
+    DestroyWindow(hCheckBox1); //Удаление CheckBox, чтобы заново нарисовать в другом месте
+    hCheckBox1 = CreateWindowEx(NULL, L"BUTTON", L"Checkbox_1", WS_CHILD | WS_VISIBLE | BS_CHECKBOX,
+        rectClient.right - 190, 90, 110, 30, hGraphPage, (HMENU)ID_CHECKBOX_1, NULL, NULL);
+    if (state == BST_CHECKED) SendMessage(hCheckBox1, BM_SETCHECK, BST_CHECKED, 0);
+
+    state = SendMessage(hCheckBox2, BM_GETCHECK, 0, 0);
+    DestroyWindow(hCheckBox2); //Удаление CheckBox, чтобы заново нарисовать в другом месте
+    hCheckBox2 = CreateWindowEx(NULL, L"BUTTON", L"Checkbox_2", WS_CHILD | WS_VISIBLE | BS_CHECKBOX,
+        rectClient.right - 190, 120, 110, 30, hGraphPage, (HMENU)ID_CHECKBOX_2, NULL, NULL);
+    if (state == BST_CHECKED) SendMessage(hCheckBox2, BM_SETCHECK, BST_CHECKED, 0);
+
+    state = SendMessage(hCheckBox3, BM_GETCHECK, 0, 0);
+    DestroyWindow(hCheckBox3); //Удаление CheckBox, чтобы заново нарисовать в другом месте
+    hCheckBox3 = CreateWindowEx(NULL, L"BUTTON", L"Checkbox_3", WS_CHILD | WS_VISIBLE | BS_CHECKBOX,
+        rectClient.right - 190, 150, 110, 30, hGraphPage, (HMENU)ID_CHECKBOX_3, NULL, NULL);
+    if (state == BST_CHECKED) SendMessage(hCheckBox3, BM_SETCHECK, BST_CHECKED, 0);
+
+    // Освобождение шрифта
+    DeleteObject(hFont);
+
+}
--- WindowsProject4/GraphPage.h
+++ WindowsProject4/GraphPage.h
@@ -1,1 +1,63 @@
-#pragma once
+//#pragma once
+
+#include <windows.h>
+#include <CommCtrl.h> // Для использования элемента управления TabControl
+#include <vector> // Для работы с данными для графиков и диаграмм
+#include <cmath> // Для математических функций
+#include <tchar.h>
+
+#pragma comment(lib, "Comctl32.lib") // Подключаем Comctl32.lib
+
+#define IDC_TABCONTROL 1001 // Примерный номер идентификатора для TabControl
+#define ID_CHECKBOX_1 2001
+#define ID_CHECKBOX_2 2002
+#define ID_CHECKBOX_3 2003
+
+// Глобальные переменные
+extern HINSTANCE hInst;
+extern HWND hTabControl;
+extern HWND hGraphPage;
+extern HWND hHistogramPage;
+extern HWND hPieChartPage;
+extern HWND hButton;
+extern HWND hCheckBox1;
+extern HWND hCheckBox2;
+extern HWND hCheckBox3;
+extern HWND* CheckBox[3];
+extern WNDPROC g_pTabCtrlProc; // Объявление и инициализация указателя
+extern WNDPROC g_pGraphPageProc; // Объявление и инициализация указателя
+extern WNDPROC g_pHistogramPageProc; // Объявление и инициализация указателя
+extern WNDPROC g_pPieChartPageProc; // Объявление и инициализация указателя
+
+// Прототипы функций
+//BOOL InitInstance(HINSTANCE hInstance, int nCmdShow);
+//LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//LRESULT CALLBACK TabControlProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+/// <summary>
+/// Процедура окна
+/// </summary>
+/// <param name="hWnd"></param>
+/// <param name="message"></param>
+/// <param name="wParam"></param>
+/// <param name="lParam"></param>
+/// <returns></returns>
+LRESULT CALLBACK GraphPageProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+//LRESULT CALLBACK HistogramPageProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+//LRESULT CALLBACK PieChartPageProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+void line(HDC hdc, int Xs, int Ys, int Xf, int Yf); // рисование отрезка прямой линии
+
+void DrawGraph(HDC hdc, RECT rectClient, double** x, int n, int numrow);
+void DrawTextOnGraphPage(HDC hdc, RECT rectClient); // текст
+void DrawHistogram(HWND hWnd);
+void DrawPieChart(HWND hWnd);
+double** getData(int size);
+//int p = 0;
+
+
+extern double** x; // массив данных, глобальный
+
+extern const int NUM; // Примерное количество точек для графика
+
+extern HDC hdc;
